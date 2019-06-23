9316e0d677f692af4eabd28531e1445c91e83181
donghyeKim <48975954+donghyeKim@users.noreply.github.com>
2019-06-24 02:46:05 +0900

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+objects = models.Manager() 
+# Create your models here.
---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+objects = models.Manager() 
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+    objects = models.Manager() 
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:120]
